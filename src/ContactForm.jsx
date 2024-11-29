6d3c5b387388666db168a844b2ac4ebfee4790c2
trmeade <trmsd@yahoo.com>
2024-11-29 14:57:20 -0500

    First implementation of Contact Form

--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -3,7 +3,50 @@ export function ContactForm() {
 
   return (
     <>
-      <h1>Contact Form</h1>
+      <h1 id="contact">Contact</h1>
+      {/* <form action="https://formspree.io/f/xldeeoyq" method="POST"> */}
+      <form className="contact-form" action="" method="">
+        <div className="form-row">
+          <input className="form-input"
+            required
+            type="text"
+            name="first-name"
+            id="first-name"
+            placeholder="First Name"
+          />
+        </div>
+        <div className="form-row">
+          <input className="form-input"
+            required
+            type="text"
+            name="last-name"
+            id="last-name"
+            placeholder="Last Name"
+          />
+        </div>
+        <div className="form-row">
+          <input className="form-input"
+            required
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+          />
+        </div>
+        <div className="form-row">
+          <textarea className="form-input"
+            required
+            name="message"
+            id="message"
+            cols="30"
+            rows="10"
+            placeholder="What would you like to say?"
+          ></textarea>
+        </div>
+        <div className="form-row">
+          <input className="btn" type="submit" value="Submit"/>
+        </div>
+      </form>
     </>
   )
 }
